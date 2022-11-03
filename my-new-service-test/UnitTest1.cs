905a7c432f88b5153ed16df30e5fddcce4177695
Beckiversen <emiliversen11@live.dk>
2022-11-03 11:09:08 +0100

    1

---
+++ my-new-service-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace my_new_service_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
